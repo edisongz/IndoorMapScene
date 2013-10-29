1dcb15e847cd168d0540ab7faee2eae2d7c863ee
edisongz <apple@appletekiMac-mini.local>
2013-10-29 10:39:07 +0800

    Initial Commit

---
+++ IndoorMapScene/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  IndoorMapScene
+//
+//  Created by apple on 13-10-29.
+//  Copyright (c) 2013年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ IndoorMapScene/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  IndoorMapScene
+//
+//  Created by apple on 13-10-29.
+//  Copyright (c) 2013年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
